7c5b0df9c42d5e214c00da0ee9b75fa0bc740fb0
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-05-17 10:01:48 -0300

    add estudos

---
+++ ConsoleApp-Conversoes/Program.cs
@@ -0,0 +1,61 @@
+﻿//conversao implicita
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+
+
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
+
+
+decimal myDecimal2 = 3.14m;
+Console.WriteLine($"decimal: {myDecimal2}");
+int myInt2 = (int)myDecimal2;
+Console.WriteLine($"int: {myInt2}");
+
+
+decimal myDecimal3 = 1.23456789m;
+float myFloat = (float)myDecimal;
+Console.WriteLine($"Decimal: {myDecimal3}");
+Console.WriteLine($"Float  : {myFloat}");
+
+//toString
+int first2 = 5;
+int second2 = 7;
+string message = first2.ToString() + second2.ToString();
+Console.WriteLine(message);
+
+//Parse
+string first3 = "5";
+string second3 = "7";
+int sum = int.Parse(first3) + int.Parse(second3);
+Console.WriteLine(sum);
+
+//Convert
+string value1 = "5";
+string value2 = "7";
+int result2 = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result2);
+
+
+int value3 = (int)1.5m; // casting truncates
+Console.WriteLine(value3);
+int value4 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value4);
+
+//tryParse
+//string value5 = "102";
+string value5 = "bad";
+int result3 = 0;
+if (int.TryParse(value5, out result3))
+{
+    Console.WriteLine($"Measurement: {result3}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result3}");
---
+++ ConsoleApp-MetodosAuxiliares/Program.cs
@@ -0,0 +1,74 @@
+﻿//metodo Sort
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+//metodo Reverse
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+//metodo Clear e Resize
+string[] pallets2 = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets2, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets2.Length}");
+foreach (var pallet in pallets2)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets2, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets2.Length}");
+
+pallets2[4] = "C01";
+pallets2[5] = "C02";
+
+foreach (var pallet in pallets2)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+//redimensionar array usando o Resize
+Console.WriteLine("");
+Array.Resize(ref pallets2, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets2.Length}");
+
+foreach (var pallet in pallets2)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+//toChar + Reverse
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
+
+//Join e Split
+string value2 = "abc123";
+char[] valueArray2 = value2.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result2 = String.Join(",", valueArray2);
+Console.WriteLine(result2);
+
+string[] items = result2.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+
+
